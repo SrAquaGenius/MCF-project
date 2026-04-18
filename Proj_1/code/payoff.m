9060d725e451cfd9566326f194b4dbc77639b104
Coiass22 <gabrielcoias5@gmail.com>
2026-04-18 20:10:10 +0100

    new payoff

--- Proj_1/code/payoff.m
+++ Proj_1/code/payoff.m
@@ -12,7 +12,7 @@ if type == "Eu" && op == "Put"
 else if type == "Eu" && op == "Call"
     res = max(S-K, 0);
 %how will I define this V_cont.
-
+% we will define equally for American and European option.
 end
 
 end
